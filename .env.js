39b7d0413808b01e65351d80eec1159badd9a210
Cristopher Alvear <c_alvear@live.cl>
2021-05-30 13:59:56 -0400

    chore: fx

--- .env.js
+++ .env.js
@@ -4,33 +4,33 @@ const app = require('./package.json');
 // each object represents
 // a execution environment
 module.exports = Promise.resolve({
-  	// default variables for all environments.
-	default: {
-		PORT: 4001,
-		VERSION: app.version,
-		TITLE: app.title,
-    	DESCRIPTION: app.description,
-	},
+    // default variables for all environments.
+    default: {
+        PORT: 4001,
+        VERSION: app.version,
+        TITLE: app.title,
+        DESCRIPTION: app.description
+    },
 
-  	// used on tests running.
-	test: {
-	},
+    // used on tests running.
+    test: {
+    },
 
-	// used on project building.
-	build: {
-		PORT: 80
-	},
+    // used on project building.
+    build: {
+        PORT: 80
+    },
 
-  	debug: {
-		ENV: 'debug'
-	},
-	development: {
-		ENV: 'development'
-	},
-	qa: {
-		ENV: 'qa'
-	},
-	production: {
-		ENV: 'production'
-	},
+    debug: {
+        ENV: 'debug'
+    },
+    development: {
+        ENV: 'development'
+    },
+    qa: {
+        ENV: 'qa'
+    },
+    production: {
+        ENV: 'production'
+    }
 });
