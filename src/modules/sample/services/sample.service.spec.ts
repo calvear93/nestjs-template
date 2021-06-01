86df5d37199b727d2442a63835bba3329023630b
Cristopher Alvear <c_alvear@live.cl>
2021-06-01 19:13:15 -0400

    chore: tests added

---
+++ src/modules/sample/services/sample.service.spec.ts
@@ -0,0 +1,26 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { SampleService } from './sample.service';
+
+describe('SampleService', () =>
+{
+    let service: SampleService;
+
+    beforeEach(async () =>
+    {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [ SampleService ]
+        }).compile();
+
+        service = module.get<SampleService>(SampleService);
+    });
+
+    it('should be defined', () =>
+    {
+        expect(service).toBeDefined();
+    });
+
+    it('should return', () =>
+    {
+        expect(service.sample()).toBe('Hello World');
+    });
+});
