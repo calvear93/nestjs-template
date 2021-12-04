b730899d42a1e9ef57a82ce615d62df8842d4bc6
Cristopher Alvear <caalvearc@achs.cl>
2021-12-04 19:34:34 -0300

    chore(swagger): some tweaks

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -16,5 +16,13 @@ export const swaggerInit = (app: INestApplication) =>
 
     const document = SwaggerModule.createDocument(app, config);
 
-    SwaggerModule.setup(process.env.API_PREFIX, app, document);
+    SwaggerModule.setup(process.env.API_PREFIX, app, document, {
+        customCssUrl: 'https://cdn.jsdelivr.net/npm/swagger-ui-themes@3.0.1/themes/3.x/theme-flattop.css',
+        customSiteTitle: process.env.TITLE,
+        swaggerOptions: {
+            tryItOutEnabled: true,
+            persistAuthorization: true,
+            displayRequestDuration: true
+        }
+    });
 };
