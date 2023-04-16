7ccb3f9c09f2f65f755c32cd136683e1759cbb53
Cristopher Alvear <calvear93@gmail.com>
2023-04-16 16:56:10 -0400

    chore: ⚡️ tweaks

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,17 @@ import { ZodValidationPipe } from './libs/zod/zod.pipe.js';
 import { MainModule } from './app/main.module.js';
 import { swaggerInit } from './app/config/swagger.config.js';
 
-const DEV = process.env.NODE_ENV === 'development';
 const PORT = +process.env.PORT;
 const PREFIX = process.env.API_PREFIX;
 const SWAGGER_UI = process.env.SWAGGER_UI === 'true';
+const onStart = () => {
+	// eslint-disable-next-line no-console
+	console.log(
+		`\n \x1B[32m➜\x1B[0m Local: \x1B[36mhttp://localhost:${PORT}/${PREFIX}`,
+	);
+};
 
+// app initializing
 const app = await NestFactory.create(MainModule, { cors: true });
 
 app.enableVersioning();
@@ -16,13 +22,5 @@ app.useGlobalPipes(new ZodValidationPipe());
 
 if (SWAGGER_UI) swaggerInit(app);
 
-await app.listen(
-	PORT,
-	// use '0.0.0.0' here as "hostname" arg if you use Fastify adapter
-	() =>
-		DEV &&
-		// eslint-disable-next-line no-console
-		console.log(
-			`\n \x1B[32m➜\x1B[0m Local: \x1B[36mhttp://localhost:${PORT}/${PREFIX}`,
-		),
-);
+// use '0.0.0.0' as second arg if you use Fastify adapter
+await app.listen(PORT, onStart);
