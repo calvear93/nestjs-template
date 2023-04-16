df68a9c715d7e9b9c15607a22c7cdc9fcbc17a81
Cristopher Alvear <calvear93@gmail.com>
2023-04-15 20:44:48 -0400

    refactor: ♻️ tweaks

--- src/main.ts
+++ src/main.ts
@@ -3,21 +3,26 @@ import { ZodValidationPipe } from './libs/zod/zod.pipe.js';
 import { MainModule } from './app/main.module.js';
 import { swaggerInit } from './app/config/swagger.config.js';
 
+const DEV = process.env.NODE_ENV === 'development';
+const PORT = +process.env.PORT;
+const PREFIX = process.env.API_PREFIX;
+const SWAGGER_UI = process.env.SWAGGER_UI === 'true';
+
 const app = await NestFactory.create(MainModule, { cors: true });
 
 app.enableVersioning();
-app.setGlobalPrefix(process.env.API_PREFIX);
+app.setGlobalPrefix(PREFIX);
 app.useGlobalPipes(new ZodValidationPipe());
 
-if (process.env.SWAGGER_UI === 'true') swaggerInit(app);
+if (SWAGGER_UI) swaggerInit(app);
 
 await app.listen(
-	+process.env.PORT,
+	PORT,
 	// use '0.0.0.0' here as "hostname" arg if you use Fastify adapter
 	() =>
-		process.env.NODE_ENV === 'development' &&
+		DEV &&
 		// eslint-disable-next-line no-console
 		console.log(
-			`\n \x1B[32m➜\x1B[0m Local: \x1B[36mhttp://localhost:${process.env.PORT}/${process.env.API_PREFIX}`,
+			`\n \x1B[32m➜\x1B[0m Local: \x1B[36mhttp://localhost:${PORT}/${PREFIX}`,
 		),
 );
