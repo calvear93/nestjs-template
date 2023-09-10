6442a639970b50638ff166c10c33249cbe07df49
Cristopher Alvear <calvear93@gmail.com>
2023-09-10 14:19:06 -0300

    chore: twks

--- src/env.d.ts
+++ src/env.d.ts
@@ -6,9 +6,6 @@ declare global {
 			readonly ENV: 'dev' | 'release';
 			readonly DEBUG?: string;
 
-			// SECTION: runtime environment
-			readonly HOSTNAME: string;
-
 			// SECTION: project info from package.json
 			readonly VERSION: string;
 			readonly PROJECT: string;
@@ -26,52 +23,11 @@ declare global {
 			readonly LANG: string;
 
 			// SECTION: api security
-			SECURITY_ENABLED?: 'true' | 'false';
-			SECURITY_HEADER_NAME: string; // name of the api key header
-			SECURITY_API_KEY: string; // value of the api key header
+			readonly SECURITY_ENABLED?: 'true' | 'false';
+			readonly SECURITY_HEADER_NAME: string; // name of the api key header
+			readonly SECURITY_API_KEY: string; // value of the api key header
 		}
 	}
-
-	// SECTION: built-in types tweaks
-	interface JSON {
-		parse<T = unknown>(
-			text: string,
-			reviver?: (this: any, key: string, value: any) => any,
-		): T;
-	}
-
-	type ObjectKeys<T> = T extends object
-		? (keyof T)[]
-		: T extends number
-		? []
-		: T extends any[] | string
-		? string[]
-		: never;
-
-	interface ObjectConstructor {
-		keys<T>(o: T): ObjectKeys<T>;
-	}
-
-	// SECTION: global custom types
-	type decimal = number;
-
-	type seconds = number;
-
-	type millis = number;
-
-	type password = string;
-
-	type bytes = number;
-
-	type uuid = string;
-
-	// unix timestamp, number of seconds that have elapsed since January 1, 1970
-	type epoch = seconds;
-
-	type AnyError = unknown;
-
-	// https://en.wikipedia.org/wiki/ISO_8601#Durations
-	type DurationISO8601 = string;
 }
 
 export {};
---
+++ src/types.d.ts
@@ -0,0 +1,46 @@
+/// <reference lib="dom" />
+
+declare global {
+	// SECTION: built-in types tweaks
+	interface JSON {
+		parse<T = unknown>(
+			text: string,
+			reviver?: (this: any, key: string, value: any) => any,
+		): T;
+	}
+
+	type ObjectKeys<T> = T extends object
+		? (keyof T)[]
+		: T extends number
+		? []
+		: T extends any[] | string
+		? string[]
+		: never;
+
+	interface ObjectConstructor {
+		keys<T>(o: T): ObjectKeys<T>;
+	}
+
+	// SECTION: global custom types
+	type decimal = number;
+
+	type seconds = number;
+
+	type millis = number;
+
+	type password = string;
+
+	type bytes = number;
+
+	type uuid = string;
+
+	// unix timestamp, number of seconds that have elapsed since January 1, 1970
+	type epoch = seconds;
+
+	type AnyError = unknown;
+
+	// https://en.wikipedia.org/wiki/ISO_8601#Durations
+	type DurationISO8601 = string;
+}
+
+export {};
