06cd4941307f2378761fe1f1d81031c4b6575fb8
Cristopher Alvear <calvear93@gmail.com>
2023-12-19 20:42:46 -0300

    chore: twks

--- src/libs/zod/index.ts
+++ src/libs/zod/index.ts
@@ -1,3 +1,7 @@
-export { ZodDto, registerDtoOpenApiSchemas } from './zod-dto.ts';
+export {
+	ZodDto,
+	ZodIterableDto,
+	registerDtoOpenApiSchemas,
+} from './zod-dto.ts';
 export { zodToJsonSchema } from './json-schema.factory.ts';
 export { ZodValidationPipe } from './zod.pipe.ts';
