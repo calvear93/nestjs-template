16160a0eaf2255a18daa47d7e939b38f38c5dccb
calvear93 <calvear93@gmail.com>
2024-06-09 14:18:12 -0400

    fix: epoch zod validator

--- src/libs/zod/validators/epoch.type.ts
+++ src/libs/zod/validators/epoch.type.ts
@@ -2,29 +2,29 @@ import { z } from 'zod';
 
 const EPOCH_REGEXP = /^(?:\/date\()?(?<epoch>\d+)(?:\)\/)?$/iv;
 
+const addTransformIssue = (context: z.RefinementCtx, value: unknown) => {
+	context.addIssue({
+		code: z.ZodIssueCode.invalid_date,
+		message: `Expected a valid Unix Timestamp, received '${value}'`,
+	});
+
+	return false as any;
+};
+
 export const epoch = () =>
 	z
 		.string()
 		.transform<Date>((value, context) => {
-			(context as any).originalValue = value;
 			const matcher = EPOCH_REGEXP.exec(value);
 
-			if (!matcher || !matcher.groups) return null as any;
+			if (!matcher || !matcher.groups)
+				return addTransformIssue(context, value);
 
 			const date = new Date(0);
 			date.setUTCSeconds(+matcher.groups.epoch);
 
 			return date;
 		})
-		.superRefine((value, context) => {
-			const originalValue = (context as any).originalValue as unknown;
-
-			if (!value) {
-				context.addIssue({
-					code: z.ZodIssueCode.invalid_date,
-					message: `Expected a valid Unix Timestamp, received '${originalValue}'`,
-				});
-
-				return z.INVALID;
-			}
+		.superRefine((value) => {
+			if (!value) return z.INVALID;
 		});
