9caf32bad89028f6023532263473a0aceb28e3bf
calvear93 <calvear93@gmail.com>
2024-10-30 14:22:56 -0300

    chore: up

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { start } from './app/app.ts';
 const PORT = +process.env.PORT;
 const BASE_URL = process.env.BASE_URL;
 const SWAGGER_ENABLED = process.env.SWAGGER_UI === 'true';
-const SECURITY_ENABLED = process.env.SECURITY_ENABLED === 'true';
 
 // application init
 const { dispose } = await start({
@@ -11,17 +10,6 @@ const { dispose } = await start({
 	prefix: BASE_URL,
 	swagger: SWAGGER_ENABLED,
 });
-
-// application features logging
-const logFeatureStatus = (name: string, enabled: boolean) => {
-	const bullet = enabled ? '\u001B[32m⦿' : '\u001B[31m⦿';
-	const message = enabled ? '\u001B[32mON' : '\u001B[31mOFF';
-
-	console.info(`  ${bullet}\u001B[0m ${name}: ${message}\u001B[0m`);
-};
-
-logFeatureStatus(' 📑 Swagger', SWAGGER_ENABLED);
-logFeatureStatus(' 🔒 Api Key', SECURITY_ENABLED);
 
 if (import.meta.env.DEV) {
 	console.info(
