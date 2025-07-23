647e206bd8e61a51c57d21b140a87b9fcbc00f49
calvear93 <calvear93@gmail.com>
2025-07-23 09:14:36 -0400

    refactor: enhance SampleSchema with metadata for fields and schema description

--- src/app/modules/sample/schemas/sample.dto.ts
+++ src/app/modules/sample/schemas/sample.dto.ts
@@ -1,9 +1,11 @@
 import { ZodObjectDto } from '#libs/zod';
 import { z } from 'zod';
 
-const SampleSchema = z.object({
-	id: z.coerce.number(),
-	name: z.string(),
-});
+const SampleSchema = z
+	.object({
+		id: z.coerce.number(),
+		name: z.string().meta({ description: 'Sample name' }),
+	})
+	.meta({ description: 'Sample DTO schema' });
 
 export class SampleDto extends ZodObjectDto(SampleSchema, 'Sample') {}
